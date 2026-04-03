3aded4b5249257f8de64b87d868dc4dba044502b
Mahir Khan <the.mahir.khan@icloud.com>
2026-04-03 11:45:27 +0530

    Added optimal soln with comments

--- MinNoOfDaysToMakeMBouquets.cpp
+++ MinNoOfDaysToMakeMBouquets.cpp
@@ -95,15 +95,72 @@ int minDays1(vector<int> &arr, int flowers, int bouquets)
     return -1;
 }
 
+/*
+
+2. Algorithm Used : Optimal
+   Time Complexity : O(log (max - min) * N), N = arr.size()
+   Auxiliary Space Requirement : O(1)
+   Intuition : The range is from arr(min) to arr(max),
+   use binary search to narrow down the range
+   Note : if there are less flowers than total flowers required
+    (arr.size() < flowers*bouquets) its not possible to make bouquets
+*/
+
+int minDays2(vector<int> &arr, int flowers, int bouquets)
+{
+    int n = arr.size();
+
+    if (n < flowers * bouquets)
+    {
+        return -1;
+    }
+
+    int max = arr[0], min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    int low = min, high = max;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        bool possibility = possibleDay(arr, mid, flowers, bouquets);
+
+        if (possibility)
+        {
+            high = mid - 1;
+        }
+
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
 int main()
 {
     vector<int> arr = {1, 2, 3, 12, 13, 2, 3, 4, 14, 15};
     display(arr);
 
     int flowers = 3;
-    int bouquets = 3;
+    int bouquets = 2;
 
-    int ans = minDays1(arr, flowers, bouquets);
+    int ans = minDays2(arr, flowers, bouquets);
 
     if (ans != -1)
     {
